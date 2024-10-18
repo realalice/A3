4663dca81195e95cf0f4fd1c8ef2f7d62199f6f7
Alan Cheng <alan7cheng@gmail.com>
2024-10-17 18:42:20 -0700

    remove useSpotifyTracks

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import { Button, StyleSheet, SafeAreaView, Text } from "react-native";
 import { useSpotifyAuth, useSpotifyTracks } from "./utils";
 import { Themes } from "./assets/Themes";
 
+// You'll need these when fetching the tracks!
+import getEnv from "./utils/env";
+import { getMyTopTracks, getAlbumTracks } from "./utils/apiOptions";
+
 export default function App() {
   const { token, getSpotifyAuth } = useSpotifyAuth();
-  const tracks = useSpotifyTracks(token);
+  const [tracks, setTracks] = useState(null);
+  
+  /* TODO: Once you are able to retrieve the token, figure out how
+   * to fetch the tracks and store them in the `tracks` state variable */
 
   return (
     <SafeAreaView style={styles.container}>
